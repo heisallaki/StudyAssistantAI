1901af9a409b1a64427c587335a32737284b1fd6
heisallaki <alvinkipslangat@gmail.com>
2026-09-11 12:16:36 +0300

    fix admin model initialization and migration imports

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -10,7 +10,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.core.config import get_settings
 from app.db.base import Base
-from app.models.init import *
+from app.models import *
 
 config = context.config
 
